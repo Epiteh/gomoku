963f7c08c5e4746915a7f51a32ca00cbf238a1db
hodooos <thomas.rochefeuille@epitech.eu>
2024-11-24 20:14:58 +0100

    ✨ update documentation for Gomoku and Minimax algorithm implementation

--- docs/main_page.h
+++ docs/main_page.h
@@ -15,109 +15,50 @@ nbc signature powered by love.
 
 #pragma once
 
-/*! \mainpage Author : Epiteh NBC Signature.
- * ## What's R-Type ?
- * R-Type is a horizontal-scrolling shoot 'em up arcade game produced by Irem in 1987.
- * The player controls a space fighter named the R-9 to defend humanity against a mysterious powerful alien life-form known as the Bydo. \n
- * The game is made up of several levels, each with a boss at the end that must be defeated to progress to the next level.
+/**
+ * @mainpage Gomoku
+ * # Gomoku
  *
- * \image html rtype.png "R-Type illustration" width=800 height=400
+ * ## How we implemented Minimax algorithm in Gomoku
  *
- * ## The project
- * The project is a remake of the R-Type game, with a client-Nerwork-server architecture.
- * Our project can be played in multiplayer mode, with up to 4 players. \n
- * The game is divided into three parts:
- * The \b client is the graphical interface, the \b server manages the game, and the \b network (protocol) manages the communication between the two. \n 
+ * ### Minimax algorithm
  *
- * * \b The \b server used an AES encryption to secure the communication between the client and the server. \n
- *                    The main architecture of the server is based on the ECS pattern. \n
- * * \b The \b client is a graphical interface, using SFML, that allows the player to interact with the game.
- * * \b The \b network is the communication part between the client and the server. It's based on the UDP protocol.
+ * The Minimax algorithm is a decision rule used in artificial intelligence for minimizing the possible loss for a worst case scenario. It is used in two-player games, such as Gomoku, to determine the best move for a player, assuming that the opponent will also play optimally.
  *
- * The entire project is written in C++ and work on Linux and Windows. You can find the Windows and Linus installers just below :
+ * ### Our implementation
  *
- *  * \b Windows: \link https://we.tl/t-9krHI4wJTp \endlink
+ * The implementation provided is a C++ class named Minimax that is designed to perform the Minimax algorithm with alpha-beta pruning for a board game. The Minimax algorithm is a decision-making algorithm used in two-player games to minimize the possible loss for a worst-case scenario. Here is a detailed explanation of the l
  *
- *  * \b Linux: \link https://we.tl/t-3kmMI2wMBd \endlink
+ * #### Class Initialization
+ * - The Minimax class is initialized with a pointer to the game board and the size of the board. The board is represented as a one-dimensional array, and the size is the length of one side of the square board.
  *
- * ## How the architecture look like ?
+ * #### Checking for Available Moves
+ * - The isMovesLeft method checks if there are any available moves left on the board. It iterates through the board and returns true if it finds any empty cell (represented by 0), otherwise returns false.
  *
- * \image html ECS_Diagram.png "R-Type architecture illustration" height=500
+ * #### Evaluating the Board
+ * - The evaluate method calculates a score for the current state of the board. It checks all possible lines of five consecutive cells (rows, columns, and diagonals) and sums up their values. The evaluateLine method is used to assign scores to these lines based on the sum of their values:
+ *  - A line with a sum of 5 (all cells occupied by the maximizing player) gets a high positive score.
+ *  - A line with a sum of -5 (all cells occupied by the minimizing player) gets a high negative score.
+ *  - Partial lines (with sums of 4, 3, 2, etc.) get progressively smaller scores.
  *
- * EntityFactory is the class that creates entities. It is used to create entities with components and systems.<br>
- * component_manager is the class that manages the components. It is used to register components, add components to entities, and get components from entities.<br>
- * ControlTow is the class that manages the game. It is used to create/delete new player, update the background and call the input system that are responsaible for inputs handling.<br>
- * GarbageCollector is the class that manages the entities. It is used to delete entities that are not used anymore.<br>
- * Core is the class that initializes the game. It is used to initialize the components, systems, events, and the stage.<br>
- * IException and its successor classes are used to handle exceptions.<br><br>
+ * #### Minimax Algorithm with Alpha-Beta Pruning
+ * - The minimax method implements the Minimax algorithm with alpha-beta pruning. It recursively evaluates the possible moves up to a certain depth:
+ *  - If the depth is 0 or there are no moves left, it returns the evaluated score of the board.
+ *  - If it is the maximizing player's turn, it tries to maximize the score by exploring all possible moves, updating the alpha value, and pruning branches where beta is less than or equal to alpha.
+ *  - If it is the minimizing player's turn, it tries to minimize the score by exploring all possible moves, updating the beta value, and pruning branches where beta is less than or equal to alpha.
  *
- * Systems are the part of the ECS that will allow you to act on the components of your entities. Here is an example of a system :<br>
- * ecs::AI : The AI system is responsible for updating the AI of the game, move the enemy according to a movement pattern.<br>
- * ecs::Collision : The Collision system is responsible for checking the collision between entities.<br>
- * ecs::Input : The Input system is responsible for handling the inputs of the player (chose the direction).<br>
- * ecs::CustomOfficer : The CustomOfficer system is responsible for blocking the user from going out of the screen.<br>
- * ecs::Health : The health system is responsible for updating the health of the entities (Player, Enemy).<br>
- * ecs::Render : The Render system is responsible for sending rendering data to the client.<br>
- * ecs::Position : The Position system is responsible for updating the position of the entities.<br>
- * ecs::Logging : The Logging system is responsible for logging the game data.<br>
+ * #### Checking Move Scope
+ * - The isInScope method checks if a move is within the scope of the current game state. It ensures that the move is adjacent to an already occupied cell, making the algorithm more efficient by focusing on relevant moves.
  *
- * You can find a more detailed explanation on the use of the architecture in the Related Pages section.
+ * #### Finding the Best Move
+ * - The get_best_move method finds the best possible move for the maximizing player. It iterates through all possible moves, uses the minimax method to evaluate each move, and keeps track of the move with the highest score.
  *
- * ## Dependencies
- * The following dependencies are required to build and run the project:
- * * \b SFML(2.6.1): Simple and Fast Multimedia Library, used for the graphical interface.
- * * \b Boost(1.86.0): Boost is a set of libraries for the C++ programming language that provides support for tasks and structures such as linear algebra, pseudorandom number generation, multithreading, image processing, regular expressions, and unit testing.
- * * \b CMake(3.26.3+): CMake is an open-source, cross-platform family of tools designed to build, test and package software.
- * * \b C++17: The project is written in C++17.
- * * \b GTest: Google Test is a unit testing library for the C++ programming language, based on the xUnit architecture.
- * * \b zlib: zlib is a software library used for data compression.
+ * This implementation efficiently evaluates the game board and determines the best move using the Minimax algorithm with alpha-beta pruning, ensuring optimal decision-making in a two-player game scenario.
  *
- * ## How to run the program (Developer Linux)
+ * ### Conclusion
  *
- * Clone the repository and go to the root of the project.
- * You can compile the program with the following steps:
- * \code{.sh}
- * $> mkdir build
- * \endcode
- * \code {.sh}
- * $> cd build
- * \endcode
- * \code {.sh}
- * $> cmake ..
- * \endcode
- * \code {.sh}
- * $> make
- * \endcode
+ * #### Why we chose Minimax algorithm
  *
- * When the program is compiled, you can launch it with the following command:
- * \code {.sh}
- * $> ./rtype_server
- * \endcode
+ * The Minimax algorithm is a widely used decision rule in two-player games due to its ability to minimize the possible loss for a worst-case scenario. By exploring all possible moves and their outcomes, the algorithm ensures that the player makes the best move based on the assumption that the opponent will also play optimally.
  *
- * \code {.sh}
- * $> ./rtype_client
- * \endcode
- *
- * ## How to run the program (Developer Windows)
- *
- * Clone the repository and go to the root of the project.
- * You can compile the program with the following steps:
- * \code {.powershell}
- * $> mkdir build
- * \endcode
- * \code {.powershell}
- * $> cmake --presets=default
- * \endcode
- * \code {.powershell}
- * $> cmake --build build
- * \endcode
- *
- * When the program is compiled, you can launch it with the following command:
- * \code {.powershell}
- * $> ./build/rtype_server
- * \endcode
- *
- * \code {.powershell}
- * $> ./build/rtype_client
- * \endcode
  */
